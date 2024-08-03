f3e49d706681654a20b506cfe2b8a0b7b93bbf80
abayzv <abayzv@gmail.com>
2024-08-03 12:12:57 +0700

    init role & permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,35 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'admin@admin.com',
-            'password' => 'admin'
+        $superadmin = User::factory()->create([
+            'name' => 'Superadmin',
+            'email' => 'superadmin@admin.com',
+            'password' => 'superadmin@789'
         ]);
+
+        $user1 = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => 'P@ssw0rd'
+        ]);
+
+        $user2 = User::factory()->create([
+            'name' => 'Driver 1',
+            'email' => 'driver1@driver.com',
+            'password' => 'driver123'
+        ]);
+
+        $role1 = Role::create(['name' => 'superadmin']);
+        $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'driver']);
+        $permission1 = Permission::create(['name' => 'view dashboard']);
+        $permission2 = Permission::create(['name' => 'create trips']);
+
+        $permission1->givePermissionTo(($role1));
+        $permission2->givePermissionTo(($role2));
+
+        $superadmin->assignRole('superadmin');
+        $user1->assignRole('admin');
+        $user2->assignRole('driver');
     }
 }
