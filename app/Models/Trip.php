385f07ef34a380149db0e130b4c20df2140790a7
abayzv <abayzv@gmail.com>
2024-08-03 12:42:17 +0700

    init trip model, controller, migration, factory, and seeder

---
+++ app/Models/Trip.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'departure_date',
+        'departure_time',
+        'stock'
+    ];
+}
---
+++ database/factories/TripFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Trip>
+ */
+class TripFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 3,
+            'departure_date' => fake()->date(),
+            'departure_time' => fake()->time(),
+            'stock' => fake()->numberBetween(200, 300)
+        ];
+    }
+}
